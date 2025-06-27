87084776bc22dffae9d7377f4a98f205790527ec
eladser <54028171+eladser@users.noreply.github.com>
2025-06-27 20:59:22 +0300

    Create dedicated Real-time Monitoring page for navigation

---
+++ src/pages/Monitoring.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import RealtimeMonitoring from '../components/enhanced/RealtimeMonitoring';
+
+const MonitoringPage: React.FC = () => {
+  return <RealtimeMonitoring />;
+};
+
+export default MonitoringPage;
