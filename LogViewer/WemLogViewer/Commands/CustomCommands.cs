7c8b8051cb791e9502500f763fb5665528d64a97
eladser <54028171+eladser@users.noreply.github.com>
2025-06-29 10:24:36 +0300

    Add missing CustomCommands class

---
+++ LogViewer/WemLogViewer/Commands/CustomCommands.cs
@@ -0,0 +1,13 @@
+using System.Windows.Input;
+
+namespace WemLogViewer;
+
+public static class CustomCommands
+{
+    public static readonly RoutedUICommand Refresh = new(
+        "Refresh", "Refresh", typeof(CustomCommands),
+        new InputGestureCollection { new KeyGesture(Key.F5) });
+        
+    public static readonly RoutedUICommand ClearFilter = new(
+        "Clear Filter", "ClearFilter", typeof(CustomCommands));
+}
