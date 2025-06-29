c2f86f0d3eec7a43c2a3825dd59c3d31279bb919
eladser <54028171+eladser@users.noreply.github.com>
2025-06-29 10:33:55 +0300

    Ensure TextBoxBehaviors namespace is correct and properly exposed

--- LogViewer/WemLogViewer/Behaviors/TextBoxBehaviors.cs
+++ LogViewer/WemLogViewer/Behaviors/TextBoxBehaviors.cs
@@ -1,1 +1,88 @@
-using System.Windows;\nusing System.Windows.Controls;\nusing System.Windows.Media;\n\nnamespace WemLogViewer.Behaviors;\n\npublic static class TextBoxBehaviors\n{\n    public static readonly DependencyProperty WatermarkProperty =\n        DependencyProperty.RegisterAttached(\n            \"Watermark\",\n            typeof(string),\n            typeof(TextBoxBehaviors),\n            new PropertyMetadata(string.Empty, OnWatermarkChanged));\n\n    public static string GetWatermark(DependencyObject obj)\n    {\n        return (string)obj.GetValue(WatermarkProperty);\n    }\n\n    public static void SetWatermark(DependencyObject obj, string value)\n    {\n        obj.SetValue(WatermarkProperty, value);\n    }\n\n    private static void OnWatermarkChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)\n    {\n        if (d is TextBox textBox)\n        {\n            textBox.GotFocus -= TextBox_GotFocus;\n            textBox.LostFocus -= TextBox_LostFocus;\n            textBox.TextChanged -= TextBox_TextChanged;\n\n            if (!string.IsNullOrEmpty((string)e.NewValue))\n            {\n                textBox.GotFocus += TextBox_GotFocus;\n                textBox.LostFocus += TextBox_LostFocus;\n                textBox.TextChanged += TextBox_TextChanged;\n                \n                UpdateWatermark(textBox);\n            }\n        }\n    }\n\n    private static void TextBox_GotFocus(object sender, RoutedEventArgs e)\n    {\n        if (sender is TextBox textBox)\n        {\n            UpdateWatermark(textBox);\n        }\n    }\n\n    private static void TextBox_LostFocus(object sender, RoutedEventArgs e)\n    {\n        if (sender is TextBox textBox)\n        {\n            UpdateWatermark(textBox);\n        }\n    }\n\n    private static void TextBox_TextChanged(object sender, TextChangedEventArgs e)\n    {\n        if (sender is TextBox textBox)\n        {\n            UpdateWatermark(textBox);\n        }\n    }\n\n    private static void UpdateWatermark(TextBox textBox)\n    {\n        var watermark = GetWatermark(textBox);\n        \n        if (string.IsNullOrEmpty(textBox.Text) && !textBox.IsFocused)\n        {\n            textBox.Foreground = Brushes.Gray;\n            textBox.Text = watermark;\n        }\n        else if (textBox.Text == watermark && textBox.IsFocused)\n        {\n            textBox.Foreground = SystemColors.ControlTextBrush;\n            textBox.Text = string.Empty;\n        }\n        else if (!string.IsNullOrEmpty(textBox.Text) && textBox.Text != watermark)\n        {\n            textBox.Foreground = SystemColors.ControlTextBrush;\n        }\n    }\n}
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace WemLogViewer.Behaviors;
+
+public static class TextBoxBehaviors
+{
+    public static readonly DependencyProperty WatermarkProperty =
+        DependencyProperty.RegisterAttached(
+            "Watermark",
+            typeof(string),
+            typeof(TextBoxBehaviors),
+            new PropertyMetadata(string.Empty, OnWatermarkChanged));
+
+    public static string GetWatermark(DependencyObject obj)
+    {
+        return (string)obj.GetValue(WatermarkProperty);
+    }
+
+    public static void SetWatermark(DependencyObject obj, string value)
+    {
+        obj.SetValue(WatermarkProperty, value);
+    }
+
+    private static void OnWatermarkChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        if (d is TextBox textBox)
+        {
+            textBox.GotFocus -= TextBox_GotFocus;
+            textBox.LostFocus -= TextBox_LostFocus;
+            textBox.TextChanged -= TextBox_TextChanged;
+
+            if (!string.IsNullOrEmpty((string)e.NewValue))
+            {
+                textBox.GotFocus += TextBox_GotFocus;
+                textBox.LostFocus += TextBox_LostFocus;
+                textBox.TextChanged += TextBox_TextChanged;
+                
+                UpdateWatermark(textBox);
+            }
+        }
+    }
+
+    private static void TextBox_GotFocus(object sender, RoutedEventArgs e)
+    {
+        if (sender is TextBox textBox)
+        {
+            UpdateWatermark(textBox);
+        }
+    }
+
+    private static void TextBox_LostFocus(object sender, RoutedEventArgs e)
+    {
+        if (sender is TextBox textBox)
+        {
+            UpdateWatermark(textBox);
+        }
+    }
+
+    private static void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+    {
+        if (sender is TextBox textBox)
+        {
+            UpdateWatermark(textBox);
+        }
+    }
+
+    private static void UpdateWatermark(TextBox textBox)
+    {
+        var watermark = GetWatermark(textBox);
+        
+        if (string.IsNullOrEmpty(textBox.Text) && !textBox.IsFocused)
+        {
+            textBox.Foreground = Brushes.Gray;
+            textBox.Text = watermark;
+        }
+        else if (textBox.Text == watermark && textBox.IsFocused)
+        {
+            textBox.Foreground = SystemColors.ControlTextBrush;
+            textBox.Text = string.Empty;
+        }
+        else if (!string.IsNullOrEmpty(textBox.Text) && textBox.Text != watermark)
+        {
+            textBox.Foreground = SystemColors.ControlTextBrush;
+        }
+    }
+}
