f4233efc3e8d19fe6e24a5f2b5ef7e883831bea9
eladser <54028171+eladser@users.noreply.github.com>
2025-06-30 10:27:24 +0300

    Add GlobalUsings.cs to resolve namespace ambiguities

---
+++ LogViewer/WemLogViewer/GlobalUsings.cs
@@ -0,0 +1,13 @@
+// Global using statements to resolve ambiguities
+global using System;
+global using System.Collections.Generic;
+global using System.Linq;
+global using System.Text;
+global using System.Threading.Tasks;
+
+// Explicitly use WPF types to avoid conflicts
+global using Clipboard = System.Windows.Clipboard;
+global using MessageBox = System.Windows.MessageBox;
+global using Color = System.Windows.Media.Color;
+global using ColorConverter = System.Windows.Media.ColorConverter;
+global using Button = System.Windows.Controls.Button;
